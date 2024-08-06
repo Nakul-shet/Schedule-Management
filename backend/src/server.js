11d36660d6524efd654b42998ee90b41161953c2
Nakul N Shet <nakulnshet1@gmail.com>
2024-08-06 19:26:14 +0530

    Added main routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const cors = require("cors");
 const {database_connection} = require("./config/database");
 
+const userRouter = require("./routes/userRoutes");
+const appointmentRouter = require("./routes/appointmentRoutes");
+
 const app = express();
 
 app.use(express.json());
@@ -11,6 +14,8 @@ app.use(cors());
 
 database_connection();
 
+app.use("/user" , userRouter)
+app.use("/appointment" , appointmentRouter)
 
 const PORT = 3001;
 app.listen(PORT , () => {
