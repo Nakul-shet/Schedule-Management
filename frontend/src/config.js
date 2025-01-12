80be386c08c0709a538319bbd1baeb289b902618
Nakul Shet <75607333+Nakul-shet@users.noreply.github.com>
2025-01-12 22:51:12 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   runEndpoint: {
-    backendUrl: 'http://ec2-43-204-228-26.ap-south-1.compute.amazonaws.com:4000',
-    authUrl: 'http://ec2-43-204-228-26.ap-south-1.compute.amazonaws.com:4000',
+    backendUrl: 'http://shourya.org:4000',
+    authUrl: 'http://shourya.org:4000/',
   },
 };
