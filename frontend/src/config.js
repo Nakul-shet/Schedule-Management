2a600bf855575111b9740787e1332331d6cb9282
Nakul Shet <75607333+Nakul-shet@users.noreply.github.com>
2025-01-12 17:00:14 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   runEndpoint: {
-    backendUrl: 'http://localhost:4000',
-    authUrl: 'http://localhost:4000',
+    backendUrl: 'http://ec2-43-204-228-26.ap-south-1.compute.amazonaws.com:4000',
+    authUrl: 'http://ec2-43-204-228-26.ap-south-1.compute.amazonaws.com:4000',
   },
 };
