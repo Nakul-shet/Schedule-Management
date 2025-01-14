da3c6ae72df1b3953b6acfa44799a623e8e705fb
Nakul Shet <75607333+Nakul-shet@users.noreply.github.com>
2025-01-14 19:30:09 +0530

    Fixed Backend Redirection error

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,17 +1,6 @@
-// export const CONFIG = {
-//   runEndpoint: {
-//     backendUrl: 'http://ec2-43-204-228-26.ap-south-1.compute.amazonaws.com:4000',
-//     authUrl: 'http://ec2-43-204-228-26.ap-south-1.compute.amazonaws.com:4000',
-//   },
-// };
 export const CONFIG = {
   runEndpoint: {
-<<<<<<< HEAD
-    backendUrl: 'http://localhost:4000',
-    authUrl: 'http://localhost:4000',
-=======
     backendUrl: 'https://shourya.org',
     authUrl: 'https://shourya.org',
->>>>>>> c7b2642a01b1909f4b796fdb65047928f0a540df
   },
 };
